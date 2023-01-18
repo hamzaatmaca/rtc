27b9fb4126b1c985b74bcd5fa5dd26fe06ad2e21
hamzaatmaca <mentalkod09@gmail.com>
2023-01-18 12:14:59 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+
+//PORT
+const PORT = process.env.PORT || 3030;
+
+//CREATE SERVER
+const server = http.createServer(app);
+
+//SOCKET SERVER
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+  cors: {
+    origins: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+//USE ENV FILE
+require("dotenv").config();
+
+//CORS OPTIONS
+app.use(cors());
+
+//CLIENT COUNT
+
+//GENERATE UNIQ ROOM
+io.engine.generateId = (req) => {
+  return uuidv4();
+};
+
+io.on("connection", (socket) => {
+  socket.on("room", (data) => {
+    console.log("room-ID", data);
+    socket.join(data);
+  });
+
+  const count = io.engine.clientsCount;
+
+  socket.on("chatMessage", (data) => {
+    socket.to(data.room).emit("chatMessageReturn", data);
+  });
+
+  socket.on("webRTC-signaling", (data) => {
+    socket.to(data.room).emit("webRTC-signaling", data);
+  });
+
+  if (count > 2) {
+    socket.emit("error", { message: "reach the limit of connections" });
+    socket.disconnect();
+    console.log("Disconnected...");
+  }
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnnect`);
+    socket.broadcast.emit("callEnded");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
